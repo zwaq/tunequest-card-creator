e3c8207c3396128ba0b2a76af729bd48bcf53162
zwaq <20557068+zwaq@users.noreply.github.com>
2024-12-21 11:47:33 +0100

    fix: remove selected value that causes a warning

--- src/App.tsx
+++ src/App.tsx
@@ -142,7 +142,7 @@ When creating your playlist you need to pay attention to select the original tra
                               className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                               onChange={e => setCodeType(e.target.value)}
                           >
-                              <option value="qr" selected>QR Code</option>
+                              <option value="qr">QR Code</option>
                               <option value="spotify">Spotify Code</option>
                           </select>
                       </div>
